af15c48fc656147fdbd9cce515dfab49226346ca
yuta1125tp <yuta1125tp@gmail.com>
2021-08-11 23:29:41 +0900

    wip

--- day03/MikanLoaderPkg/Main.c
+++ day03/MikanLoaderPkg/Main.c
@@ -2,6 +2,7 @@
 #include <Library/UefiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/PrintLib.h>
+#include <Library/MemoryAllocationLib.h>
 #include <Protocol/LoadedImage.h>
 #include <Protocol/SimpleFileSystem.h>
 #include <Protocol/DiskIo2.h>
@@ -142,13 +143,56 @@ EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root)
     return EFI_SUCCESS;
 }
 
+EFI_STATUS OpenGOP(EFI_HANDLE image_handle,
+                   EFI_GRAPHICS_OUTPUT_PROTOCOL **gop)
+{
+    UINTN num_gop_handles = 0;
+    EFI_HANDLE *gop_handles = NULL;
+    gBS->LocateHandleBuffer(
+        ByProtocol,
+        &gEfiGraphicsOutputProtocolGuid,
+        NULL,
+        &num_gop_handles,
+        &gop_handles);
+    gBS->OpenProtocol(
+        gop_handles[0],
+        &gEfiGraphicsOutputProtocolGuid,
+        (VOID **)gop,
+        image_handle,
+        NULL,
+        EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+    FreePool(gop_handles);
+
+    return EFI_SUCCESS;
+}
+
+const CHAR16 *GetPixelFormatUnicode(EFI_GRAPHICS_PIXEL_FORMAT fmt)
+{
+    switch (fmt)
+    {
+    case PixelRedGreenBlueReserved8BitPerColor:
+        return L"PixelRedGreenBlueReserved8BitPerColor";
+    case PixelBlueGreenRedReserved8BitPerColor:
+        return L"PixelBlueGreenRedReserved8BitPerColor";
+    case PixelBitMask:
+        return L"PixelBitMask";
+    case PixelBltOnly:
+        return L"PixelBltOnly";
+    case PixelFormatMax:
+        return L"PixelFormatMax";
+    default:
+        return L"InvalidPixelFormat";
+    }
+}
+
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
     EFI_SYSTEM_TABLE *system_table)
 {
     Print(L"Hello, Mikan World!!\n");
 
-    // #@@range_begin(main)
+#pragma region dump_memmap
     CHAR8 memmap_buf[4096 * 4];
     struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
     GetMemoryMap(&memmap);
@@ -163,10 +207,30 @@ EFI_STATUS EFIAPI UefiMain(
 
     SaveMemoryMap(&memmap, memmap_file);
     memmap_file->Close(memmap_file);
-    // #@@range_end(main)
+#pragma endregion dump_memmap
 
-    // #@@range_begin(read_kernel)
-    EFI_FILE_PROTOCOL *kernel_file;
+#pragma region gop
+    EFI_GRAPHICS_OUTPUT_PROTOCOL *gop;
+    OpenGOP(image_handle, &gop);
+    Print(L"resolution %ux%u, Pixel Format: %s, %u pixles/line\n",
+          gop->Mode->Info->HorizontalResolution,
+          gop->Mode->Info->VerticalResolution,
+          GetPixelFormatUnicode(gop->Mode->Info->PixelFormat),
+          gop->Mode->Info->PixelsPerScanLine);
+    Print(L"Frame Buffer: 0x%0lx - 0x%0lx, Size: %lu bytes\n",
+          gop->Mode->FrameBufferBase,
+          gop->Mode->FrameBufferBase + gop->Mode->FrameBufferSize,
+          gop->Mode->FrameBufferSize);
+    UINT8 *frame_buffer = (UINT8 *)gop->Mode->FrameBufferBase;
+    for (UINTN i = 0; i < gop->Mode->FrameBufferSize; ++i)
+    {
+        frame_buffer[i] = 255;
+    }
+#pragma endregion gop
+
+#pragma region read_kernel
+    EFI_FILE_PROTOCOL *
+        kernel_file;
     root_dir->Open(
         root_dir,
         &kernel_file,
@@ -195,7 +259,7 @@ EFI_STATUS EFIAPI UefiMain(
         &kernel_file_size,
         (VOID *)kernel_base_addr);
     Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
-    // #@@range_end(read_kernel)
+#pragma endregion read_kernel
 
 #pragma region exist_bs
     EFI_STATUS status;
