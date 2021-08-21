328dd6f8883e12350fbf92efe956e62daf2296cc
yuta1125tp <yuta1125tp@gmail.com>
2021-08-21 13:02:32 +0900

    day05d

--- day04/kernel/font.cpp
+++ day04/kernel/font.cpp
@@ -52,3 +52,20 @@ void WriteAscii(PixelWriter &writer, int x, int y, char c, const PixelColor &col
         }
     }
 }
+
+/**
+ * @brief 書式なし文字列の表示
+ * 
+ * @param writer 
+ * @param x 
+ * @param y 
+ * @param s 
+ * @param color 
+ */
+void WriteString(PixelWriter &writer, int x, int y, const char *s, const PixelColor &color)
+{
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        WriteAscii(writer, x + 8 * i, y, s[i], color);
+    }
+}
